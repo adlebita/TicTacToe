c992dca6f0bc5f5dcbc4b3386db19946414b29ad
lebit <lebit@DESKTOP-ED5DCUC>
2021-04-22 13:59:15 +1200

    Final completed

--- TicTacToeGame/src/game/Board.java
+++ TicTacToeGame/src/game/Board.java
@@ -4,28 +4,32 @@ import java.util.*;
 public class Board {
 
 	Scanner myScanner = new Scanner(System.in);
-	int count = 1;
+	String playerCoordinate;
 	int playerRow;
 	int playerColumn;
+	boolean inputValidation = false;
+	boolean playerTurn = true;
 	String[][] myBoard = new String[3][3];
 	
 	public void playBoard() {
 		setBoard();
 		printBoard();
-		playGame();	
+		do{
+			playGame();	
+		} while (!checkBoard());
 	}	
 	
 	public String[][] setBoard(){
 		
-		myBoard[0][0] = " ";
-		myBoard[0][1] = " ";
-		myBoard[0][2] = " ";
-		myBoard[1][0] = " ";
-		myBoard[1][1] = " ";
-		myBoard[1][2] = " ";
-		myBoard[2][0] = " ";
-		myBoard[2][1] = " ";
-		myBoard[2][2] = " ";
+		myBoard[0][0] = "1";
+		myBoard[0][1] = "2";
+		myBoard[0][2] = "3";
+		myBoard[1][0] = "4";
+		myBoard[1][1] = "5";
+		myBoard[1][2] = "6";
+		myBoard[2][0] = "7";
+		myBoard[2][1] = "8";
+		myBoard[2][2] = "9";
 		
 		return this.myBoard;
 	}
@@ -48,25 +52,74 @@ public class Board {
 		}
 		return myBoard;
 	}
-			
-	public int playerMove() {
+
 	
-		String playerCoordinate = myScanner.nextLine();
-					
-		char[] value = playerCoordinate.toCharArray();
-		
-		int playerRow = Character.getNumericValue(value[0]);
-		int playerColumn = Character.getNumericValue(value[1]);
-			
-		this.playerRow = playerRow; 
-		this.playerColumn = playerColumn;
-		return this.playerRow + this.playerColumn;
+	public void playerLocation() {
+		System.out.println("Please enter a location");
+		playerCoordinate = myScanner.nextLine();
 	}
 	
-	public boolean markerPlacement(int row, int column) {
+	public void inputValidation() {
+//		if (playerCoordinate > 0 && playerCoordinate < 10) {
+			switch(playerCoordinate) {
+			case "1":
+				playerRow = 0;
+				playerColumn = 0;
+				inputValidation = markerCheck(playerRow, playerColumn);
+				break;
+			case "2":
+				playerRow = 0;
+				playerColumn = 1;
+				inputValidation = markerCheck(playerRow, playerColumn);
+				break;
+			case "3":
+				playerRow = 0;
+				playerColumn = 2;
+				inputValidation = markerCheck(playerRow, playerColumn);
+				break;
+			case "4":
+				playerRow = 1;
+				playerColumn = 0;
+				inputValidation = markerCheck(playerRow, playerColumn);
+				break;
+			case "5":
+				playerRow = 1;
+				playerColumn = 1;
+				inputValidation = markerCheck(playerRow, playerColumn);
+				break;
+			case "6":
+				playerRow = 1;
+				playerColumn = 2;
+				inputValidation = markerCheck(playerRow, playerColumn);
+				break;
+			case "7":
+				playerRow = 2;
+				playerColumn = 0;
+				inputValidation = markerCheck(playerRow, playerColumn);
+				break;
+			case "8":
+				playerRow = 2;
+				playerColumn = 1;
+				inputValidation = markerCheck(playerRow, playerColumn);
+				break;
+			case "9":
+				playerRow = 2;
+				playerColumn = 2;
+				inputValidation = markerCheck(playerRow, playerColumn);
+				break;
+			default:
+				System.out.println("Try again.");
+				break;
+			}
+	
+	}
+	
+	
+	
+	public boolean markerCheck(int row, int column) {
 
 		if(myBoard[playerRow][playerColumn] == "X" || myBoard[playerRow][column] == "O") {
-			System.out.println("Please enter another location");
+			System.out.println("That spot is taken. Please choose another location");
 			return false;
 		}
 		return true;
@@ -105,24 +158,26 @@ public class Board {
 	}
 		return false;
 	}
-
-	public void playGame() {
-	while(checkBoard() == false) {
-	//	System.out.println("Enter coordinates");
-		do {
-		playerMove();
-		} while(markerPlacement(playerRow, playerColumn) == false);
-		if(count % 2 == 0) {
-			count++;
-			System.out.println("CROSSES turn... Enter a location");
-			myBoard[playerRow][playerColumn] = "O";
-		} else {
-			count++;
-			System.out.println("NOUGHTS turn... Enter a location");
+	
+	public void placeMarker() {
+		if(playerTurn) {
 			myBoard[playerRow][playerColumn] = "X";
-		}
-		checkBoard();
-		printBoard();
+			playerTurn = false;
+		} else {
+			myBoard[playerRow][playerColumn] = "O";
+			playerTurn = true;
 		}
 	}
+
+	public void playGame() {
+	do {
+		playerLocation();
+		inputValidation();
+	} while (!inputValidation);
+		placeMarker();
+		checkBoard();
+		printBoard();
+	}
+	
+	
 }
--- TicTacToeGame/src/game/Main.java
+++ TicTacToeGame/src/game/Main.java
@@ -1,17 +1,13 @@
 package game;
-import java.util.*;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		
 		System.out.println("Welcome to NOUGHTS & CROSSES");
-		System.out.println("To begin please enter a location. Crosses go first.");
+		System.out.println("Crosses go first.");
 		Board tictactoe  = new Board();
 		tictactoe.playBoard();
-		sc.close();
 	}
-
 }
